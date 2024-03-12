1c7352f64b7d11136a0d65e8575ea5902fc6d3dd
Alvaro Cabrera <100432481@alumnos.uc3m.es>
2024-03-12 18:45:19 +0100

    dx_commands done

---
+++ Engine/graphics/directX12/dx_device.cpp
@@ -0,0 +1,14 @@
+/************************************************************************
+ * Copyright (c) 2024 Alvaro Cabrera Barrio
+ * This code is licensed under MIT license (see LICENSE.txt for details) 
+ ************************************************************************/
+/**
+ * @file dx_device.cpp
+ * @version 1.0
+ * @date 12/03/2024
+ * @brief Short description
+ *
+ * Longer description
+ */
+
+#include "dx_device.hpp"
---
+++ Engine/graphics/directX12/dx_device.hpp
@@ -0,0 +1,25 @@
+/************************************************************************
+ * Copyright (c) 2024 Alvaro Cabrera Barrio
+ * This code is licensed under MIT license (see LICENSE.txt for details) 
+ ************************************************************************/
+/**
+ * @file dx_device.hpp
+ * @version 1.0
+ * @date 12/03/2024
+ * @brief Short description
+ *
+ * Longer description
+ */
+
+#ifndef REVEAL3DENGINE_DX_DEVICE_HPP
+#define REVEAL3DENGINE_DX_DEVICE_HPP
+
+
+
+class dx_device {
+
+};
+
+
+
+#endif //REVEAL3DENGINE_DX_DEVICE_HPP
