216d8b22fe4775acf6c0849b34c89199c08444ab
ch0pex <100431481@alumnos.uc3m.es>
2025-01-14 20:57:06 +0100

    PSO and root refactor

--- graphics/directX12/dx_root_signature.hpp
+++ graphics/directX12/dx_root_signature.hpp
@@ -18,33 +18,6 @@
 
 namespace reveal3d::graphics::dx12 {
 
-/*
-class RootParameter {
-public:
-    RootParameter();
-
-        ~RootParameter()
-        {
-            Clear();
-        }
-
-        void clear();
-        void InitAsConstants( u32 reg, u32 words, D3D12_SHADER_VISIBILITY visibility = D3D12_SHADER_VISIBILITY_ALL, u32
-Space = 0 ); void InitAsConstantBuffer( u32 reg, D3D12_SHADER_VISIBILITY visibility = D3D12_SHADER_VISIBILITY_ALL, u32
-Space = 0 ); void InitAsBufferSRV( u32 reg, D3D12_SHADER_VISIBILITY visibility = D3D12_SHADER_VISIBILITY_ALL, u32 Space
-= 0 ); void InitAsBufferUAV( u32 reg, D3D12_SHADER_VISIBILITY visibility = D3D12_SHADER_VISIBILITY_ALL, u32 Space = 0 );
-        void InitAsDescriptorRange( D3D12_DESCRIPTOR_RANGE_TYPE type, u32 reg, u32 count, D3D12_SHADER_VISIBILITY
-visibility = D3D12_SHADER_VISIBILITY_ALL, u32 spcae = 0 ); void InitAsDescriptorTable( u32 RangeCount,
-D3D12_SHADER_VISIBILITY visibility = D3D12_SHADER_VISIBILITY_ALL ); void SetTableRange( u32 rangeIndex,
-D3D12_DESCRIPTOR_RANGE_TYPE type, u32 reg, u32 countData, u32 space = 0 );
-
-        const D3D12_ROOT_PARAMETER& operator() ( void ) const { return parameter_; }
-
-private:
-    D3D12_ROOT_PARAMETER parameter_;
-};
-*/
-
 class RootSignature {
 public:
   explicit RootSignature(u32 num_root_params = 0);
