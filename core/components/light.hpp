1618ec3344220e2e35aae82f05479af5b7d5d05d
ch0pex <100431481@alumnos.uc3m.es>
2025-02-10 16:09:35 +0100

    Light component work in progress

--- core/components/light.hpp
+++ core/components/light.hpp
@@ -23,14 +23,49 @@ struct Light : Component<Light> {
 
   using Component::Component;
 
-  void active() const {
-    pool().toggleActive(id_);
-    setDirty();
-  }
-
   void color(math::vec3 const color) const {
     pool().light(id_).color = color;
     setDirty();
+  }
+
+  void position(math::vec3 const position) const {
+    if (pool().lightType(id_) != render::Light::type::directional) {
+      pool().light(id_).position = position;
+      setDirty();
+    }
+    else {
+      logger(LogWarning) << "Setting position property to a directional light, this action will be ignored";
+    }
+  }
+
+  void direction(math::vec3 const direction) const {
+    if (pool().lightType(id_) == render::Light::type::directional) {
+      pool().light(id_).direction = direction;
+      setDirty();
+    }
+    else {
+      logger(LogWarning) << "Setting direction property to a non directional light, this action will be ignored";
+    }
+  }
+
+  void fallOffStart(f32 const power) const {
+    if (pool().lightType(id_) != render::Light::type::directional) {
+      pool().light(id_).spot_power = power;
+      setDirty();
+    }
+    else {
+      logger(LogWarning) << "Setting fall off start property to a directional light, this action will be ignored";
+    }
+  }
+
+  void fallOffEnd(f32 const power) const {
+    if (pool().lightType(id_) != render::Light::type::directional) {
+      pool().light(id_).spot_power = power;
+      setDirty();
+    }
+    else {
+      logger(LogWarning) << "Setting fall off end property to a directional light, this action will be ignored";
+    }
   }
 
   void spotPower(f32 const power) const {
@@ -38,14 +73,19 @@ struct Light : Component<Light> {
       pool().light(id_).spot_power = power;
       setDirty();
     }
+    else {
+      logger(LogWarning) << "Setting spot power property to a non stop light, this action will be ignored";
+    }
   }
 
-  void direction(math::vec3 const direction) const {
-    pool().light(id_).direction = direction;
+  void active(bool act) const {
+    pool().light(id_).active = act;
     setDirty();
   }
 
-  [[nodiscard]] bool isActive() const { pool().isActive(id_); }
+  bool active() const { return pool().light(id_).active; }
+
+  render::Light const& properties() const { return pool().light(id_); }
 
   [[nodiscard]] render::Light::type type() const { return pool().lightType(id_); }
 };
